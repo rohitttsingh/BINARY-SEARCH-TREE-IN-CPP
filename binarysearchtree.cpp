d326fbfb765085262d3a1458b17d5bcd8ec9a414
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-07-10 14:44:21 +0530

    Update binarysearchtree.cpp

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -1,86 +1,87 @@
-#include<bits/stdc++.h>
+#include <c++/v1/bits/stdc++.h>
+
 #include<iostream>
 using namespace std;
 
 struct Node{
-	int data;
-	Node * left;
-	Node * right;
+    int data;
+    Node * left;
+    Node * right;
 };
 
 Node *newnode(int data){
-	
-	Node * root=new Node();
-	root->data=data;
-	root->left=root->right=NULL;
-	
-	return root;
+    
+    Node * root=new Node();
+    root->data=data;
+    root->left=root->right=NULL;
+    
+    return root;
 }
 
 Node * insert(Node * root,int data){
-	Node * newNode= new Node();
-	
-	if(root==NULL){
-		root=newnode(data);
-	}
-	else if (data<root->data){
-		root->left=insert(root->left,data);
-	}
-	else if(data>root->data){
-		root->right=insert(root->right,data);
-	}
-	return root;
+    Node * newNode= new Node();
+    
+    if(root==NULL){
+        root=newnode(data);
+    }
+    else if (data<root->data){
+        root->left=insert(root->left,data);
+    }
+    else if(data>root->data){
+        root->right=insert(root->right,data);
+    }
+    return root;
 }
 void inorder(Node * root){
-	
-	if(root!=NULL){
-		
+    
+    if(root!=NULL){
+        
 inorder(root->left);
 cout<<root->data<<" ";
 inorder(root->right);
 
-	}
+    }
 }
 void postorder(Node * root){
-	
-	if(root!=NULL){
-		
+    
+    if(root!=NULL){
+        
 postorder(root->left);
 postorder(root->right);
 cout<<root->data<<" ";
 
 
-	}
+    }
 }
 void preorder(Node * root){
-	
-	if(root!=NULL){
-		
+    
+    if(root!=NULL){
+        
 cout<<root->data<<" ";
 preorder(root->left);
 preorder(root->right);
 
-	}
+    }
 }
 
 int minroot(Node*root){
-	Node * temp=root;
-	while(temp->left!=NULL){
-		temp=temp->left;
-	}
-	cout<<"Minimum Value in BST is : "<<temp->data<<"\n";
-	
-	return temp->data;
+    Node * temp=root;
+    while(temp->left!=NULL){
+        temp=temp->left;
+    }
+    cout<<"Minimum Value in BST is : "<<temp->data<<"\n";
+    
+    return temp->data;
 }
 
 int maxroot(Node*root){
-	Node * temp=root;
-	while(temp->right!=NULL){
-		temp=temp->right;
-	}
-	cout<<"Maximum Value in BST is : "<<temp->data<<"\n";
-	
-	return temp->data;
+    Node * temp=root;
+    while(temp->right!=NULL){
+        temp=temp->right;
+    }
+    cout<<"Maximum Value in BST is : "<<temp->data<<"\n";
+    
+    return temp->data;
 
 }
 Node* minValueNode(struct Node* node)
@@ -131,69 +132,71 @@ Node* deleteNode(Node* root, int key)
 }
 
 int height(Node*root){
-	if(root==NULL) return -1;
-	
+    if(root==NULL) return -1;
+    
 return max(height(root->left),height(root->right))+1;
 
 }
 void LOT(Node *root){
-	if(root==NULL) return;
-	
-	queue<Node *> Q;
-	Q.push(root);//pusing address of root
-	while(!Q.empty()){
-		Node *current =Q.front();
-		cout<<current->data<<" ";
-		if(current->left!=NULL) Q.push(current->left);
-		if(current->right!=NULL) Q.push(current->right);
-		Q.pop();
-	}	
+    if(root==NULL) return;
+    
+    queue<Node *> Q;
+    Q.push(root);//pusing address of root
+    while(!Q.empty()){
+        Node *current =Q.front();
+        cout<<current->data<<" ";
+        if(current->left!=NULL) Q.push(current->left);
+        if(current->right!=NULL) Q.push(current->right);
+        Q.pop();
+    }
 }
 bool IsBSTorNOT(Node*root,int minv,int maxv){
-	if(root==NULL) return true;
-	
-	if(root->data<minv && root->data>maxv && IsBSTorNOT(root->left,minv,root->data) && IsBSTorNOT(root->right,root->data,maxv)){
-		return true;
-	}
-	else false;
+    if(root==NULL) return true;
+    
+    if(root->data<minv && root->data>maxv && IsBSTorNOT(root->left,minv,root->data) && IsBSTorNOT(root->right,root->data,maxv)){
+        return true;
+    }
+    else return false;
 }
 
 int main()
 { int h=0;
-	Node * root=NULL;
-	
-	root=insert(root,5);
-	root=insert(root,10);
-	root=insert(root,20);
-	root=insert(root,8);
-	root=insert(root,12);
-	root=insert(root,17);
-	root=insert(root,25);
-	root=insert(root,7);
-	root=insert(root,9);
-	
+    Node * root=NULL;
+    
+    root=insert(root,5);
+    root=insert(root,10);
+    root=insert(root,20);
+    root=insert(root,8);
+    root=insert(root,12);
+    root=insert(root,17);
+    root=insert(root,25);
+    root=insert(root,7);
+    root=insert(root,9);
+    
 cout<<"INORDER\n";
 inorder(root);
 cout<<"\nPREORDER\n";
 preorder(root);
 cout<<"\nPOSTORDER\n";
 postorder(root);
-cout<<"\n\n\FINDING MIN AND MAX OF THE BST\n";
+cout<<"\n\nFINDING MIN AND MAX OF THE BST\n";
 minroot(root);maxroot(root);
-cout<<"\n\n\FINDING THE HEIGHT OF BST\n";
+cout<<"\n\nFINDING THE HEIGHT OF BST\n";
 h=height(root);
 cout<<h;
-cout<<"\n\n\FINDING THE LEVEL ORDER TRAVERSAL OF BST\n";
+cout<<"\n\nFINDING THE LEVEL ORDER TRAVERSAL OF BST\n";
 LOT(root);
-cout<<"\n\n\FINDING THE GIVEN TREE IS A BST OR NOT\n";
+cout<<"\n\nFINDING THE GIVEN TREE IS A BST OR NOT\n";
 int ans= IsBSTorNOT(root,minroot(root),maxroot(root));
 
 if(ans==false){cout<<"\n\nTHE GIVEN TREE IS A BST";
 }else {
-	cout<<"\n\nTHE GIVEN TREE IS NOT A BST";
+    cout<<"\n\nTHE GIVEN TREE IS NOT A BST";
 }
 cout<<endl;
-root=deleteNode(root, 32);
+    cout<<" DELETE NODE 5";
+
+root=deleteNode(root, 5);
     cout<<endl;
  inorder(root);
 
